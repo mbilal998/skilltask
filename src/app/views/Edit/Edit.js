70a517885232919c1be0f61280e38435812e615c
MuhammadBilal <m.bilal998@gmail.com>
2021-01-19 17:26:10 +0500

    Functionality completed

--- src/app/views/Edit/Edit.js
+++ src/app/views/Edit/Edit.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { TextField, Container, makeStyles, Button } from '@material-ui/core';
 import { useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { info } from '../../assets/Info';
 import { useHistory } from 'react-router-dom';
+import Model from '../SkillModel/Model';
+
+import Badge from '@material-ui/core/Badge';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -32,28 +36,69 @@ export default function Edit() {
     const companyRef = useRef();
     const addressRef = useRef();
 
+    const [count, setCount] = useState(0);
+
     const updatedata = () => {
-        
+
         info[0].Name = nameRef.current.value;
         info[0].FatherName = fathernameRef.current.value;
         info[0].Designation = designationRef.current.value;
         info[0].Company = companyRef.current.value;
         info[0].Address = addressRef.current.value;
-        
         history.push('/')
-
     }
 
+    const handleCallback = (e) => {
+        if (e) {
+            setCount(count + 1);
+        } else {
+            setCount(count - 1);
+        }
+    }
+
+    function findExpertiseLevel() {
+        if (count <= 4) {
+            return (
+                <Badge color="secondary" badgeContent=" " variant="dot">
+                    Bignner
+                </Badge>
+            );
+        }
+        if (count <= 7) {
+            return (
+                <Badge color="secondary" badgeContent=" " variant="dot">
+                    mediocre
+                </Badge>
+            );
+        }
+        if (count <= 9) {
+            return (
+                <Badge color="secondary" badgeContent=" " variant="dot">
+                    expert
+                </Badge>
+            );
+        }
+    }
+
+    // TODO: React.Fragment
+
     return (
-        <Container maxWidth="sm">
-            <form className={classes.root} noValidate autoComplete="off">
-                <TextField inputRef={nameRef} id="my-name" label="Name" value={myname} onChange={(e) => setName(e.target.value)} />
-                <TextField inputRef={fathernameRef} id="my-fathername" label="FatherName" value={myfathername} onChange={(e) => setFatherName(e.target.value)} />
-                <TextField inputRef={designationRef} id="my-designation" label="Designation" value={mydesignation} onChange={(e) => setDesignation(e.target.value)} />
-                <TextField inputRef={companyRef} id="my-company" label="Company" value={mycompany} onChange={(e) => setCompany(e.target.value)} />
-                <TextField inputRef={addressRef} id="my-address" label="Address" value={myaddress} onChange={(e) => setAddress(e.target.value)} />
-            </form>
-            <Button variant="contained" color="primary" onClick={() => updatedata()}>Update</Button>
-        </Container>
+        <>
+            <Container maxWidth="sm">
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField inputRef={nameRef} id="my-name" label="Name" value={myname} onChange={(e) => setName(e.target.value)} />
+                    <TextField inputRef={fathernameRef} id="my-fathername" label="FatherName" value={myfathername} onChange={(e) => setFatherName(e.target.value)} />
+                    <TextField inputRef={designationRef} id="my-designation" label="Designation" value={mydesignation} onChange={(e) => setDesignation(e.target.value)} />
+                    <TextField inputRef={companyRef} id="my-company" label="Company" value={mycompany} onChange={(e) => setCompany(e.target.value)} />
+                    <TextField inputRef={addressRef} id="my-address" label="Address" value={myaddress} onChange={(e) => setAddress(e.target.value)} />
+                </form>
+
+                <Button variant="contained" color="primary" onClick={() => updatedata()}>Update</Button>
+                <hr></hr>
+                <Model handleCallback={handleCallback} />
+
+                <p>Your level is (count: {count}): {findExpertiseLevel()}</p>
+            </Container>
+        </>
     );
 }
---
+++ src/app/views/SkillModel/Model.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { useState } from 'react';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Container, makeStyles, Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+function Model(props) {
+
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const [state, setState] = React.useState({
+        checkedA: false,
+        checkedB: false,
+        checkedC: false,
+        checkedD: false,
+        checkedE: false,
+        checkedF: false,
+        checkedG: false,
+        checkedH: false,
+        checkedI: false,
+    });
+
+    const handleChange = (event) => {
+
+        setState({ ...state, [event.target.name]: event.target.checked });
+        props.handleCallback(event.target.checked);
+    };
+
+    return (
+        <>
+            <div>
+                <Button variant="contained" color="primary" type="button" onClick={handleOpen}>Skills</Button>
+            </div>
+            <Container maxWidth="sm">
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    className={classes.modal}
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                        timeout: 500,
+                    }}
+                >
+                    <Fade in={open}>
+                        <div className={classes.paper}>
+                            <FormGroup row>
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedA} onChange={handleChange} name="checkedA" />}
+                                    label="PHP"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedB} onChange={handleChange} name="checkedB" />}
+                                    label="HTML"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedC} onChange={handleChange} name="checkedC" />}
+                                    label="JS"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedD} onChange={handleChange} name="checkedD" />}
+                                    label="CSS"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedE} onChange={handleChange} name="checkedE" />}
+                                    label="SCSS"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedF} onChange={handleChange} name="checkedF" />}
+                                    label="REACTJS"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedG} onChange={handleChange} name="checkedG" />}
+                                    label="NODEJS"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedH} onChange={handleChange} name="checkedH" />}
+                                    label="POSTGRESS"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={state.checkedI} onChange={handleChange} name="checkedI" />}
+                                    label="LARAVEL"
+                                />
+                            </FormGroup>
+                        </div>
+                    </Fade>
+                </Modal>
+            </Container>
+        </>
+    );
+}
+
+export default Model;
